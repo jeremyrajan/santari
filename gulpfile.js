9b0cbb21a65519cb9acc3ac10f39a7fb0a53940e
Jeremy Rajan <jeremyrajan@gmail.com>
2016-12-26 20:34:53 +0530

    deploy command

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
 const path = require('path');
+const updateCheck = require('update-notifier');
+const packageJSON = require('./package.json');
+const execSync = require('child_process').execSync;
 
 gulp.task('unit_tests', () => {
   gulp.src(path.join(__dirname, 'tests', '*.test.js'), { read: false })
@@ -17,6 +20,27 @@ gulp.task('lint', (done) => {
     .pipe(eslint.format())
     .pipe(eslint.failAfterError())
     .on('finish', () => done());
+});
+
+gulp.task('deploy', () => {
+  updateCheck({
+    pkg: packageJSON,
+    updateCheckInterval: 0,
+    callback: (err, update) => {
+      if (err) {
+        return err;
+      }
+      if (update.current !== update.latest) {
+        try {
+          execSync('npm publish');
+        } catch (error) {
+          return err;
+        }
+      } else {
+        return console.log('Nothing to deploy.');
+      }
+    }
+  });
 });
 
 
