d27465e43ea564e2b18190cdc29c65cae70ae086
Jeremy <jeremyrajan@gmail.com>
2018-11-17 15:19:23 +0800

    udpate automatic publish command NPM

--- scripts/npm_login.sh
+++ scripts/npm_login.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-set -o nounset
-set -o errexit
+# set -o nounset
+# set -o errexit
 
-npm adduser <<!
-$NPM_USERNAME
-$NPM_PASSWORD
-$NPM_EMAIL
-!
+# npm adduser <<!
+# $NPM_USERNAME
+# $NPM_PASSWORD
+# $NPM_EMAIL
+# !
+echo -e "$NPM_USERNAME\n$NPM_PASSWORD\n$NPM_EMAIL" | npm login
